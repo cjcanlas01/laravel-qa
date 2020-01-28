34083c1636d831291e0b38c19c576ea051f4f26c
cjcanlas01 <cjcanlas78@gmail.com>
2020-01-28 21:59:12 +0800

    Add comment regarding str_slug availability

--- app/Question.php
+++ app/Question.php
@@ -13,7 +13,13 @@ class Question extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
-
+    /**
+     * Function str_slug has been removed on version 6.0
+     * Options are install helper package through composer or
+     * Use Laravel facade:
+     *      use Illuminate\Support\Str;
+     *      Str::slug($string);
+     */
     public function setTitleAttribute($value) {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
