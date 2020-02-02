4745131d02c8d1e4faf0302ac4a0573a6424cd20
cjcanlas01 <cjcanlas78@gmail.com>
2020-02-02 20:22:15 +0800

    Add feature to show question details

--- app/Question.php
+++ app/Question.php
@@ -27,8 +27,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        // return route('questions.show', $this->id);
-        return '#';
+        return route('questions.show', $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -46,5 +45,10 @@ class Question extends Model
         }
 
         return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
--- resources/views/layouts/_form.blade.php
+++ resources/views/layouts/_form.blade.php
@@ -1,7 +1,7 @@
 @csrf
 <div class="form-group">
     <label for="question-title">Question Title</label>
-    <input type="text" name="title" value="{{ old('title', $question->title) }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+    <input type="text" name="title" value="{{ old('title', $question->title ?? '') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
     @if ($errors->has('title'))
         <div class="invalid-feedback">
@@ -11,7 +11,7 @@
 </div>
 <div class="form-group">
     <label for="question-body">Explain your Question</label>
-    <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body', $question->body) }}</textarea>
+    <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body', $question->body ?? '') }}</textarea>
     
     @if ($errors->has('body'))
     <div class="invalid-feedback">
