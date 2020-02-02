c0657ff55f3962fe04da65a6eb37fe77a87ce36b
cjcanlas01 <cjcanlas78@gmail.com>
2020-02-03 01:56:55 +0800

    Design answers table and update colum answers of table question

--- app/Question.php
+++ app/Question.php
@@ -13,6 +13,12 @@ class Question extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+    
     /**
      * Function str_slug has been removed on version 6.0
      * Options are install helper package through composer or
@@ -37,7 +43,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if($this->best_answer_id) {
                 return "answered-accepted";
             }
--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+
 }
