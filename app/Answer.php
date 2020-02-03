3cfc15b51924253434bb0cce82a3760e399ab547
ChristianJohnGSS <christianjohn.gss@gmail.com>
2020-02-03 10:34:28 +0800

    Generate answers data

--- app/Answer.php
+++ app/Answer.php
@@ -14,4 +14,14 @@ class Answer extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer) {
+            $answer->question->increment('answers_count');
+            // $answer->question->save();
+        });
+    }
 }
